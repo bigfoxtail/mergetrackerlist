568e837cd0fcd5663df22d30ef14993e0e56e4a9
fox <bigfoxtail@hotmail.com>
2026-06-13 11:38:31 +0800

    update

--- merge_tracker_list.py
+++ merge_tracker_list.py
@@ -1,23 +1,96 @@
+import re
+
 import requests
 
 
 def download_trackerslist():
     all_trackers_list = ""
+
     res = requests.get('https://cf.trackerslist.com/all.txt')
     if res.status_code == 200:
         all_trackers_list = all_trackers_list + "\n" + res.text
+
     res = requests.get('https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt')
     if res.status_code == 200:
         all_trackers_list = all_trackers_list + "\n" + res.text
+
+    res = requests.get('https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all_ip.txt')
+    if res.status_code == 200:
+        all_trackers_list = all_trackers_list + "\n" + res.text
+
+    res = requests.get(
+        'https://newtrackon.com/api/stable?include_ipv4_only_trackers=1&include_ipv6_only_trackers=1&min_age_days=0')
+    if res.status_code == 200:
+        all_trackers_list = all_trackers_list + "\n" + res.text
+
     res = requests.get('https://torrends.to/torrent-tracker-list/?download=latest')
     if res.status_code == 200:
         all_trackers_list = all_trackers_list + "\n" + res.text
-    res = requests.get('https://www.justdailytrackers.com/tracker-snapshots.json')
+
+    res = requests.get('https://www.justdailytrackers.com/trackers.js')
     if res.status_code == 200:
-        tracker_json = res.json()
-        for tracker_info in tracker_json["items"]:
-            all_trackers_list = all_trackers_list + "\n" + tracker_info['url']
+        pattern = r'const\s+(?P<var_name>\w+)\s*=\s*`(?P<content>.*?)`'
+        matches = re.finditer(pattern, res.text, re.DOTALL)
+        extracted_data = {}
+        for match in matches:
+            var_name = match.group('var_name')
+            raw_text = match.group('content')
+            clean_lines = dict.fromkeys([
+                line.strip() for line in raw_text.splitlines() if line.strip()
+            ])
+            formatted_text = "\n\n".join(clean_lines)
+            extracted_data[var_name] = formatted_text
+        all_trackers_list = all_trackers_list + "\n" + extracted_data['allTrackers']
+
     with open('all_trackers_list.txt', 'w', encoding="utf-8") as f:
+        clean_lines = dict.fromkeys(
+            [line.strip() for line in all_trackers_list.splitlines() if line.strip()]
+        )
+        final_text = "\n\n".join(clean_lines) + "\n\n"
+        f.write(final_text)
+        print(f"成功生成去重文件")
+
+
+def download_bset_trackerslist():
+    all_trackers_list = ""
+
+    res = requests.get('https://cf.trackerslist.com/best.txt')
+    if res.status_code == 200:
+        all_trackers_list = all_trackers_list + "\n" + res.text
+
+    res = requests.get('https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt')
+    if res.status_code == 200:
+        all_trackers_list = all_trackers_list + "\n" + res.text
+
+    res = requests.get('https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best_ip.txt')
+    if res.status_code == 200:
+        all_trackers_list = all_trackers_list + "\n" + res.text
+
+    res = requests.get(
+        'https://newtrackon.com/api/stable?include_ipv4_only_trackers=1&include_ipv6_only_trackers=1&min_age_days=0')
+    if res.status_code == 200:
+        all_trackers_list = all_trackers_list + "\n" + res.text
+
+    res = requests.get('https://torrends.to/torrent-tracker-list/?download=latest')
+    if res.status_code == 200:
+        all_trackers_list = all_trackers_list + "\n" + res.text
+
+    res = requests.get('https://www.justdailytrackers.com/trackers.js')
+    if res.status_code == 200:
+        pattern = r'const\s+(?P<var_name>\w+)\s*=\s*`(?P<content>.*?)`'
+        matches = re.finditer(pattern, res.text, re.DOTALL)
+        extracted_data = {}
+        for match in matches:
+            var_name = match.group('var_name')
+            raw_text = match.group('content')
+            clean_lines = dict.fromkeys([
+                line.strip() for line in raw_text.splitlines() if line.strip()
+            ])
+            formatted_text = "\n\n".join(clean_lines)
+            extracted_data[var_name] = formatted_text
+        all_trackers_list = all_trackers_list + "\n" + extracted_data['bestTrackers']
+
+    with open('best_trackers_list.txt', 'w', encoding="utf-8") as f:
         clean_lines = dict.fromkeys(
             [line.strip() for line in all_trackers_list.splitlines() if line.strip()]
         )
@@ -28,3 +101,4 @@ def download_trackerslist():
 
 if __name__ == '__main__':
     download_trackerslist()
+    download_bset_trackerslist()
